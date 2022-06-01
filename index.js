be79b0c167f779cdca539cef59d65cf121a3e0dc
HaarisMalik <haaris.malik416@gmail.com>
2022-06-01 17:43:52 -0400

    deployment ready

--- index.js
+++ index.js
@@ -1,77 +1,48 @@
-
-
-
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalSotrage = JSON.parse(localStorage.getItem("myLead"))
+console.log(leadsFromLocalSotrage)
+
+if (leadsFromLocalSotrage) {
+    myLeads = leadsFromLocalSotrage
+    render(myLeads)
+}
+
+function render(leads){
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++){
+        listItems += `
+        <li>
+         <a target='_blank' href='${leads[i]}'>
+         ${leads[i]}
+         </a>
+        </li>
+        `
+       }
+       ulEl.innerHTML = listItems 
+    }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
 })
 
 
 
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++){
-    listItems += "<li>" +  myLeads[i] + "</li> "
-    ulEl.innerHTML = listItems 
-  
-}
 
 
 
 
-/* EVENT LISTENER 
-
-let openBox = document.getElementById("open-box")
-
-openBox.addEventListener("click", function(){
-    console.log("I want to open the box!")
-}) 
-
-function saveLead() {
-    console.log("button clicked")
-}
-
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("clicked" function(){
-    console.log("button clicked form eventlistener")
-}) */
-
-/* WHEN TO SAY LET AND CONST PRACTISE 
-
-
-const playerName = 'per'
-let credits = 45
-
-credits = credits - 10
-
-//if possible default to use consts. if not , use let
-
-
-const basePrice = 520
-const discount = 120
-let shippingCost = 12
-let shippingTime = "5-12 days"
-
-shippingCost = 15
-shippingTime = "7-4 days"
-
-const fullPrice = basePrice - discount + shippingCost
-
-console.log("Total cost: "+ fullPrice + ". it will arrive in "+ shippingTime) */
-
-/* WRITING INNERHTML TAGS 
-
-//use .innerHTML to render buy! button inside the div container
-
-const conatiner = document.getElementById("container")
-
-conatiner.innerHTML = "<button onclick='buy()>Buy!</button>"
-
-function buy() {
-    conatiner.innerHTML += "<p>Thank you for buying!</p>"
-}*/
